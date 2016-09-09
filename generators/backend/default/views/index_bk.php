2ee57539abb4089f8c7cd1fabd46c9438534e91c
kizi nhoc <nhockizi@gmail.com>
2016-09-09 10:18:25 +0700

    update

--- generators/backend/default/views/index_bk.php
+++ generators/backend/default/views/index_bk.php
@@ -7,16 +7,29 @@ $nameAttribute     = $generator->getNameAttribute();
 $excludesAttribute = [ 'is_delete', 'created_by', 'created_date', 'updated_date', 'id', 'updated_by' ];
 $idName            = Inflector::camel2id( StringHelper::basename( $generator->controllerName ), '_' );
 $Url            = Inflector::camel2id( StringHelper::basename( $generator->controllerName ), '-' );
-echo '
-<table id="'.$idName.'_table" class="display responsive nowrap" cellspacing="0" width="100%">
-</table>
-<?php
-    $this->registerJs("
-        var table = $(\'#'.$idName.'_table\').DataTable( {
-            dom: \'Bfrtip\',
-            ajax:\'".Url::to([\''.$Url.'/load-data\'])."\',
-            serverSide: true,
-            columns: [';
+echo '<?php
+use nhockizi\widgets\DataTables;
+use yii\helpers\Url;
+$this->title = '.$generator->generateString( Inflector::pluralize( Inflector::camel2words( StringHelper::basename( $generator->controllerName ) ) ) ).';
+$this->params["breadcrumbs"][] = $this->title;
+?>
+<?= 
+    DataTables::widget([
+        "id" => "'.$idName.'_table",
+        "dom" => "Bfrtip",
+        "columns" => [
+            [
+                "data" => "null",
+                "defaultContent" => "",
+                "className" => "control",
+                "orderable" => "false"
+            ],
+            [
+                "data" => "null",
+                "defaultContent" => "",
+                "className" => "select-checkbox",
+                "orderable" => "false"
+            ],';
 $i = 0;
 foreach ( $generator->getColumnNames() as $key => $attribute ):
     if (!in_array( $attribute, $excludesAttribute, true ) ):
@@ -34,60 +47,51 @@ foreach ( $generator->getColumnNames() as $key => $attribute ):
                 endif;
             endif;
         }
-        echo "\t\t\t\t{ title: '".$name."', data: '".$attribute."' , orderable : false},\n";
+        echo "\t\t\t[\n \t\t\t\t'title' => '".$name."',\n \t\t\t\t'data' => '".$attribute."' ,\n \t\t\t\t'orderable' => 'false'\n\t\t\t],\n";
         $i++;
     endif;
-endforeach;  
-echo "\t\t\t],
-            select: true,
-            buttons: [
-                [
-                    'text' : 'Create',
-                    'action' : function(e,dt,node,config){
-                        Create();
-                    }
-                ],
-                [
-                    'extend' : 'selectedSingle',
-                    'text' : 'Edit',
-                    'action' : function(e,dt,node,config){
-                        var id = dt.rows( { selected: true } ).data().pluck('id')[0];
-                        Edit(id);
-                    }
-                ],
-                [
-                    'extend' : 'selected',
-                    'text' : 'Delete',
-                    'action' : function(e,dt,node,config){
-                        var id = dt.rows( { selected: true } ).data().pluck('id')[0];
-                        Edit(id);
-                    }
-                ]
+endforeach;
+echo "\t\t\t[
+                'data' => '' ,
+                'render' => 'function ( data, type, row ) {
+                                return \' <button type=\"button\" onclick=\"Edit(\'+row.id+\');\">Edit</button> <button type=\"button\" onclick=\"Delete(\'+row.id+\');\">Delete</button>\';
+                            }',
+                'orderable' => 'false'
+            ]
+        ],\n";
+echo "\t\t'bSort' => 'false',
+        'select' => 'true',
+        'ajax' => Url::to(['".$Url."/load-data']),
+        'buttons' => [
+            [
+                'text' => 'Create',
+                'action' => 'function(e,dt,node,config){
+                    Create();
+                }'
             ],
-            'bSort' : false,
-            fixedColumns: {
-                leftColumns: 1
-            },
-            processing: true,
-            serverSide: true,
-            oLanguage: {
-                sLengthMenu: 'Hiện _MENU_ mục',
-                sSearch: 'Tìm kiếm:',
-                oPaginate: {
-                    sPrevious: 'Trước',
-                    sNext: 'Kế tiếp'
-                },
-                sEmptyTable: 'Không có dữ liệu',
-                sProcessing: 'Đang tải dữ liệu...',
-                sZeroRecords: 'Không tìm thấy dữ liệu phù hợp',
-                sInfo: 'Hiển thị _START_ đến _END_ của _TOTAL_ mục',
-                sInfoEmpty: 'Hiển thị 0 đến 0 của 0 mục',
-                sInfoFiltered: '(filtered của _MAX_ tồng số trong mục)',
-                sInfoPostFix: '',
-                sUrl: ''
-            },
-        });
-    \");
+            [
+                'extend' => 'selectedSingle',
+                'text' => 'Edit',
+                'action' => 'function(e,dt,node,config){
+                    var id = dt.rows( { selected: true } ).data().pluck(\"id\")[0];
+                    Edit(id);
+                }'
+            ],
+            [
+                'extend' => 'selected',
+                'text' => 'Delete',
+                'action' => 'function(e,dt,node,config){
+                    Delete(id);
+                }'
+            ]
+        ],
+        'paging' => 'true',
+        'info'=> 'false',
+        'searching'=> 'false',
+        'lengthChange'=> 'false',
+        'processing' => 'true',
+        'serverSide' => 'true',
+    ])
 ?>
 <script type='text/javascript'>
     function Create(){
@@ -100,5 +104,4 @@ echo "\t\t\t],
         alert(id);
     }
 </script>";
-";
 ?>
