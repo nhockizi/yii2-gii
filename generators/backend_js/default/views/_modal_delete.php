6f9012ab27921610cae79d9b1eec051db9870f13
NhocKizi <nhockizi@NhocKizis-MacBook-Pro.local>
2016-12-24 18:15:11 +0700

    update

---
+++ generators/backend_js/default/views/_modal_delete.php
@@ -0,0 +1,8 @@
+<div class="modal-body">
+	<button class="bootbox-close-button close" aria-hidden="true" data-dismiss="modal" type="button" style="margin-top: -10px;">×</button>
+	<div class="bootbox-body">Bạn có chắc muốn xóa không?</div>
+</div>
+<div class="modal-footer">
+	<button class="btn btn-default" type="button" data-dismiss="modal" data-bb-handler="cancel">Hủy</button>
+	<button class="btn btn-primary" type="button" data-bb-handler="confirm" id="btn_delete_model" data-value="<?= '<?= ' . '$id' . ' ?>' ?>"> Xóa</button>
+</div>
